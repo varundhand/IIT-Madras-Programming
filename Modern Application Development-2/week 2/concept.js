733f52483208ea6e2e97c93146e644c1beb05a7d
Varun Dhand <varundhand77@gmail.com>
2024-06-18 00:16:18 +0530

    Week 2: mad high order functions and arrays

---
+++ Modern Application Development-2/week 2/concept.js
@@ -0,0 +1,123 @@
+// Arrays 
+
+
+// What are Generators in JavaScript?
+// Generators in JavaScript are a special type of function that can be paused and resumed. 
+// They are created using a function declaration. 
+// When called, a generator function does not execute its code. 
+// Instead, it returns a generator object that can be used to control the execution of the generator function.
+
+// let x = [1, 'b', a => a+1 ]
+// x.length = 5
+
+// // for loop classic
+// for (let i=0; i<x.length; i++){ // if it were for in or for of loop, we could have used const instead of let because it creates a new instance of the variable in each iteration.
+//     console.log(`x: ${x[i]} of type ${typeof x[i]}`);
+// }
+
+// // for in loop 
+// for (const i in x){ //* it skipes the undefined values
+//     console.log(`x: ${x[i]} of type ${typeof x[i]}`);
+// }   // returns x: 1 of type number, x: b of type string, x: a => a+1 of type function
+
+// // for of loop
+// for (const i of x){
+//     console.log(`x ${i} of type ${typeof i}`);
+// }
+
+// let x = {'a': 1, b: 'b', 'c':[1,2,3]}
+
+// for (const i in x ){
+//     console.log(`keys: ${i}` )
+//     console.log(`values: ${x[i]}` )
+// }
+
+// Objects are not iterable directly 
+// Object.entries(x).map((key,value) => { // Objects.entries returns an array of key value pairs
+//     console.log(`keys: ${key}` )
+//     console.log(`values: ${value}` )    
+// })
+
+// Spread Operator 
+// let x = [1,2,3]
+// let y = [...x, 4, 5, 6] 
+    
+
+// let x = [1,2,3,4,5]
+
+//! Javascript High Order Functions and Arrays:
+// * forEach - calls a function for each element in an array
+// * Does not return a new array; primarily used for side effects.
+
+// x.forEach((item, index) => {
+//     console.log(`${index} ---> ${item}`)
+// })
+
+// * map - calls a function for each element in an array
+// * Returns a new array of the same length with the transformed elements.
+
+// const doubledX = x.map((i, index) => {
+//     return (i*2)
+// })
+
+// console.log(doubledX);
+
+// * filter - filters elements in an array based on a provided function that returns a boolean.
+// * Returns a new array with only the elements that pass the test.
+
+// const filteredX = x.filter((i) => {
+//     return i % 2 !== 0;
+// })
+
+// console.log(filteredX);
+
+// * reduce - reduces an array to a single value based on a provided function.
+// * Executes the function on each element of the array, passing the result to the next iteration.
+// * reduce function takes in an accumulator and the current value.
+
+// const items = [
+//     {name: "book", price: 100},
+//     {name: "pen", price: 30}, 
+//     {name: "pencil", price: 10},
+//     {name: "eraser", price: 20},
+// ]
+
+// const totalPrice = items.reduce((total,item) => {
+//     // console.log(total);
+//     return total + item.price
+// }, 0)  // 0 is the initial value of the accumulator i.e. total
+
+// console.log(totalPrice)
+
+// * some - checks if at least one element in an array passes a test.
+// * Returns a boolean value.
+
+// const numbers = [1,2,3,4,5,6,7,8,9,10]
+// const hasMultipleOfThree = numbers.some((i) => i % 3 === 0)
+// console.log(hasMultipleOfThree);
+
+// * every - checks if all elements in an array pass a test.
+// * Returns a boolean value.
+
+// const numbers = [1,2,3,4,5,6,7,8,9,10]
+// const allArePositive = numbers.every((i) => i > 0)
+// console.log(allArePositive);
+
+// * find - returns the first element in an array that passes a test.
+// * Returns the first element that satisfies the condition or undefined if no such element found.
+
+// const numbers = [1,2,3,4,5,6,7,8,9,10]
+// const isDivisibleBy4and8 = numbers.find(i => i%8 === 0 & i%4 === 0)
+// console.log(isDivisibleBy4and8);
+
+// * findIndex - returns the index of the first element in an array that passes a test.
+// * Returns the index of the first element that satisfies the condition or -1 if no such element found.
+
+// const numbers = [1,2,3,4,5,6,7,8,9,10]
+// const firstMultipleOf5 = numbers.findIndex(i => i%5 === 0)
+// console.log(numbers[firstMultipleOf5])
+
+
+// spread operator used as remainder
+// const [a, b, ...c] = [1,2,3,4,5,6,7,8,9]
+// console.log(c)
---
+++ Modern Application Development-2/week 2/script.js
@@ -0,0 +1,116 @@
+// /* Abstract:
+// *
+// * Write method definitions to manage Cafe.
+// *
+// * Product defines a product with its name, price and number of items in stock
+// * A cafe is initialized with an array of products, array of instances of CustomerProducts class, and balance amount
+// * A customer with a customer_id can buy things if it's available in the Cafe.
+// *
+// * The stock should be reduced based on the buy request
+// * Cafe balance amount should be updated
+// * Customer history is maintained
+// *
+// * A customer with a customer_id can return things if he/she has bought before
+// *
+// * Check in the customer_products, if he/she has bought before
+// * If yes, reduce or remove accordingly
+// * Increase the stock
+// * Update the balance
+// */
+
+// /*
+//  * buyProduct makes a purchase in the cafe, and does the following:
+//  * 1. Reduces the stock of a product based on the buy request
+//  * 2. Updates the cafe balance based on the buy request
+//  * 3. Creates and add the CustomerProducts instance to the customer_products of Cafe class
+//  *
+//  * @param {number} customer_id - unique ID of customer
+//  * @param {object} product - object of Product class
+//  * @param {number} count - number of items (quantity) to be purchased
+//  * @return {boolean} 
+//  *
+//  * The method should return true if the purchase is successful, and false in the following cases:
+//  * 1. Product is not available in Cafe
+//  * 2. The quantity to be puchased is more than the current stock of a product
+// */
+
+// /*
+//  * returnProduct makes a return of a purchase in the cafe, and does the following:
+//  * 1. Updates the stock of a particular product based on the return request
+//  * 2. Updates the cafe balance based on the return request
+//  * 3. Updates or deletes the CustomerProducts instance to the customer_products of Cafe class (delete in case of a full return)
+//  *
+//  * @param {number} customer_id - unique ID of customer
+//  * @param {object} product - object of Product class
+//  * @param {number} count - number of items (quantity) to be returned
+//  * @return {boolean} 
+//  *
+//  * The method should return true if the return is successful, and false in the following cases:
+//  * 1. No such purchase was made from the Cafe
+//  * 2. The quantity to be returned is more than the quantity purchased
+// */
+
+// /*
+//  * getCurrentBalance return the current balance of the Cafe
+// */
+
+
+// class Product {
+ 
+//     constructor(name, price, stock) {
+//       this.name = name;
+//       this.price = price;
+//       this.stock = stock;
+//     }
+  
+//   }
+  
+//   class CustomerProducts {
+  
+//     constructor(customer_id, product, count) {
+//       this.customer_id = customer_id;
+//       this.product = product;
+//       this.count = count;
+//     }
+  
+//   }
+  
+//   class Cafe {
+  
+//     constructor(products, balance) {
+//       this.products = products;
+//       this.balance = balance;
+//       //Maintain the customer history
+//       this.customer_products = [];
+//     }
+  
+  
+//     buyProduct(customer_id, product, count){
+  
+//       // Add your code here
+  
+//     }
+  
+  
+//     returnProduct(customer_id, product, count){
+     
+//       // Add your code here
+  
+//     }
+  
+  
+//     getCurrentBalance(){
+  
+//       // get the current balance at cafe
+  
+//     }
+  
+//   }
+
+// x = [1, 2, 3, 4, 5, 6]
+// y = [... x, 7, 8, 9]
+// z = y.filter ((x) => x % 2 == 0)
+//  .map ((i) => 1 * 1)
+// .reduce((a, i) => a + i, (a = 0))
+// console. log(z)
+
