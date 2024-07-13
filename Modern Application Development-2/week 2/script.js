f73740482e3a9902b4574628d5e14925af8aeb98
Varun Dhand <varundhand77@gmail.com>
2024-07-13 13:15:17 +0530

    Week 5 Clean Up

--- Modern Application Development-2/week 2/script.js
+++ Modern Application Development-2/week 2/script.js
@@ -1,155 +1,165 @@
-// /* Abstract:
-// *
-// * Write method definitions to manage Cafe.
-// *
-// * Product defines a product with its name, price and number of items in stock
-// * A cafe is initialized with an array of products, array of instances of CustomerProducts class, and balance amount
-// * A customer with a customer_id can buy things if it's available in the Cafe.
-// *
-// * The stock should be reduced based on the buy request
-// * Cafe balance amount should be updated
-// * Customer history is maintained
-// *
-// * A customer with a customer_id can return things if he/she has bought before
-// *
-// * Check in the customer_products, if he/she has bought before
-// * If yes, reduce or remove accordingly
-// * Increase the stock
-// * Update the balance
-// */
+// // /* Abstract:
+// // *
+// // * Write method definitions to manage Cafe.
+// // *
+// // * Product defines a product with its name, price and number of items in stock
+// // * A cafe is initialized with an array of products, array of instances of CustomerProducts class, and balance amount
+// // * A customer with a customer_id can buy things if it's available in the Cafe.
+// // *
+// // * The stock should be reduced based on the buy request
+// // * Cafe balance amount should be updated
+// // * Customer history is maintained
+// // *
+// // * A customer with a customer_id can return things if he/she has bought before
+// // *
+// // * Check in the customer_products, if he/she has bought before
+// // * If yes, reduce or remove accordingly
+// // * Increase the stock
+// // * Update the balance
+// // */
 
-// /*
-//  * buyProduct makes a purchase in the cafe, and does the following:
-//  * 1. Reduces the stock of a product based on the buy request
-//  * 2. Updates the cafe balance based on the buy request
-//  * 3. Creates and add the CustomerProducts instance to the customer_products of Cafe class
-//  *
-//  * @param {number} customer_id - unique ID of customer
-//  * @param {object} product - object of Product class
-//  * @param {number} count - number of items (quantity) to be purchased
-//  * @return {boolean} 
-//  *
-//  * The method should return true if the purchase is successful, and false in the following cases:
-//  * 1. Product is not available in Cafe
-//  * 2. The quantity to be puchased is more than the current stock of a product
-// */
+// // /*
+// //  * buyProduct makes a purchase in the cafe, and does the following:
+// //  * 1. Reduces the stock of a product based on the buy request
+// //  * 2. Updates the cafe balance based on the buy request
+// //  * 3. Creates and add the CustomerProducts instance to the customer_products of Cafe class
+// //  *
+// //  * @param {number} customer_id - unique ID of customer
+// //  * @param {object} product - object of Product class
+// //  * @param {number} count - number of items (quantity) to be purchased
+// //  * @return {boolean} 
+// //  *
+// //  * The method should return true if the purchase is successful, and false in the following cases:
+// //  * 1. Product is not available in Cafe
+// //  * 2. The quantity to be puchased is more than the current stock of a product
+// // */
 
-// /*
-//  * returnProduct makes a return of a purchase in the cafe, and does the following:
-//  * 1. Updates the stock of a particular product based on the return request
-//  * 2. Updates the cafe balance based on the return request
-//  * 3. Updates or deletes the CustomerProducts instance to the customer_products of Cafe class (delete in case of a full return)
-//  *
-//  * @param {number} customer_id - unique ID of customer
-//  * @param {object} product - object of Product class
-//  * @param {number} count - number of items (quantity) to be returned
-//  * @return {boolean} 
-//  *
-//  * The method should return true if the return is successful, and false in the following cases:
-//  * 1. No such purchase was made from the Cafe
-//  * 2. The quantity to be returned is more than the quantity purchased
-// */
+// // /*
+// //  * returnProduct makes a return of a purchase in the cafe, and does the following:
+// //  * 1. Updates the stock of a particular product based on the return request
+// //  * 2. Updates the cafe balance based on the return request
+// //  * 3. Updates or deletes the CustomerProducts instance to the customer_products of Cafe class (delete in case of a full return)
+// //  *
+// //  * @param {number} customer_id - unique ID of customer
+// //  * @param {object} product - object of Product class
+// //  * @param {number} count - number of items (quantity) to be returned
+// //  * @return {boolean} 
+// //  *
+// //  * The method should return true if the return is successful, and false in the following cases:
+// //  * 1. No such purchase was made from the Cafe
+// //  * 2. The quantity to be returned is more than the quantity purchased
+// // */
 
-// /*
-//  * getCurrentBalance return the current balance of the Cafe
-// */
+// // /*
+// //  * getCurrentBalance return the current balance of the Cafe
+// // */
 
 
-// class Product {
+// // class Product {
  
-//     constructor(name, price, stock) {
-//       this.name = name;
-//       this.price = price;
-//       this.stock = stock;
-//     }
+// //     constructor(name, price, stock) {
+// //       this.name = name;
+// //       this.price = price;
+// //       this.stock = stock;
+// //     }
   
-//   }
+// //   }
   
-//   class CustomerProducts {
+// //   class CustomerProducts {
   
-//     constructor(customer_id, product, count) {
-//       this.customer_id = customer_id;
-//       this.product = product;
-//       this.count = count;
-//     }
+// //     constructor(customer_id, product, count) {
+// //       this.customer_id = customer_id;
+// //       this.product = product;
+// //       this.count = count;
+// //     }
   
-//   }
+// //   }
   
-//   class Cafe {
+// //   class Cafe {
   
-//     constructor(products, balance) {
-//       this.products = products;
-//       this.balance = balance;
-//       //Maintain the customer history
-//       this.customer_products = [];
-//     }
-  
-  
-//     buyProduct(customer_id, product, count){
-  
-//       // Add your code here
-  
-//     }
+// //     constructor(products, balance) {
+// //       this.products = products;
+// //       this.balance = balance;
+// //       //Maintain the customer history
+// //       this.customer_products = [];
+// //     }
   
   
-//     returnProduct(customer_id, product, count){
+// //     buyProduct(customer_id, product, count){
+  
+// //       // Add your code here
+  
+// //     }
+  
+  
+// //     returnProduct(customer_id, product, count){
      
-//       // Add your code here
+// //       // Add your code here
   
-//     }
+// //     }
   
   
-//     getCurrentBalance(){
+// //     getCurrentBalance(){
   
-//       // get the current balance at cafe
+// //       // get the current balance at cafe
   
-//     }
+// //     }
   
+// //   }
+
+// // x = [1, 2, 3, 4, 5, 6]
+// // y = [... x, 7, 8, 9]
+// // z = y.filter ((x) => x % 2 == 0)
+// //  .map ((i) => 1 * 1)
+// // .reduce((a, i) => a + i, (a = 0))
+// // console. log(z)
+
+
+// // const a = {
+// //     'x1': 10,
+// //     func1: function() {
+// //         console.log(this.x1);
+// //     },
+// // }
+// // const b = {
+// //     'x1': 20,
+// //     func2: function(){
+// //         c = a.func1
+// //         c.apply(b, [10])
+// //     }
+// // }
+// // b.func2()
+
+// // document.getElementById('id').style.backgroundColor = 'red'
+
+
+// const person1 = {
+//   name: 'Alice',
+//   greet: function(greeting, punctuation) {
+//     console.log(greeting + ', ' + this.name + punctuation);
 //   }
+// };
 
-// x = [1, 2, 3, 4, 5, 6]
-// y = [... x, 7, 8, 9]
-// z = y.filter ((x) => x % 2 == 0)
-//  .map ((i) => 1 * 1)
-// .reduce((a, i) => a + i, (a = 0))
-// console. log(z)
+// const person2 = { name: 'Bob' };
 
+// // person1's greet method, but calling it with person2 as this
+// person1.greet.call(person2, 'Hi', '!'); // Output: "Hi, Bob!"
 
-// const a = {
-//     'x1': 10,
-//     func1: function() {
-//         console.log(this.x1);
-//     },
-// }
-// const b = {
-//     'x1': 20,
-//     func2: function(){
-//         c = a.func1
-//         c.apply(b, [10])
-//     }
-// }
-// b.func2()
+// // person1's greet method, but calling it with person2 as this and arguments as an array
+// person1.greet.apply(person2, ['Hello', '!!']); // Output: "Hello, Bob!!"
 
-// document.getElementById('id').style.backgroundColor = 'red'
+// // person1's greet method, creating a new function with person2 as this
+// const greetBob = person1.greet.bind(person2, 'Hey');
 
+// // Calling the bound function later
+// greetBob('?'); // Output: "Hey, Bob?"
 
-const person1 = {
-  name: 'Alice',
-  greet: function(greeting, punctuation) {
-    console.log(greeting + ', ' + this.name + punctuation);
-  }
-};
-
-const person2 = { name: 'Bob' };
-
-// person1's greet method, but calling it with person2 as this
-person1.greet.call(person2, 'Hi', '!'); // Output: "Hi, Bob!"
-
-// person1's greet method, but calling it with person2 as this and arguments as an array
-person1.greet.apply(person2, ['Hello', '!!']); // Output: "Hello, Bob!!"
-
-// person1's greet method, creating a new function with person2 as this
-const greetBob = person1.greet.bind(person2, 'Hey');
-
-// Calling the bound function later
-greetBob('?'); // Output: "Hey, Bob?"
+var x = 5
+let obj = {
+    'x': 10,
+    fun: x => {
+        this.x = x
+    }
+}
+obj.fun(20)
+console.log(x,'and',obj.x)
--- Programming in Python/week 4/setsVsList.py
+++ Programming in Python/week 4/setsVsList.py
@@ -1,30 +1,87 @@
-import time
+# import time
 
-# Generate a large list and set with the same elements
-large_list = list(range(1000000))
-large_set = set(large_list)
+# # Generate a large list and set with the same elements
+# large_list = list(range(1000000))
+# large_set = set(large_list)
 
-# Function to measure time taken to find an element in a list
-def search_list(data, target):
-    start_time = time.time()
-    found = target in data
-    end_time = time.time()
-    return found, end_time - start_time
+# # Function to measure time taken to find an element in a list
+# def search_list(data, target):
+#     start_time = time.time()
+#     found = target in data
+#     end_time = time.time()
+#     return found, end_time - start_time
 
-# Function to measure time taken to find an element in a set
-def search_set(data, target):
-    start_time = time.time()
-    found = target in data
-    end_time = time.time()
-    return found, end_time - start_time
+# # Function to measure time taken to find an element in a set
+# def search_set(data, target):
+#     start_time = time.time()
+#     found = target in data
+#     end_time = time.time()
+#     return found, end_time - start_time
 
-# Target element to search for
-target = 999999
+# # Target element to search for
+# target = 999999
 
-# Measure time for list
-found_list, time_list = search_list(large_list, target)
-print(f"List: Found={found_list}, Time={time_list} seconds")
+# # Measure time for list
+# found_list, time_list = search_list(large_list, target)
+# print(f"List: Found={found_list}, Time={time_list} seconds")
 
-# Measure time for set
-found_set, time_set = search_set(large_set, target)
-print(f"Set: Found={found_set}, Time={time_set} seconds")
+# # Measure time for set
+# found_set, time_set = search_set(large_set, target)
+# print(f"Set: Found={found_set}, Time={time_set} seconds")
+
+# print(1,2,3,4,5)
+# print((2 ** 3) ** 0)
+# word_list = [0,1,2,3,4,5,6,7,8,9]
+# print(word_list[3:7])
+# _a_ = 321
+# print(_a_)
+# print(int(-9.9))
+
+# alphabets = 'abcdefghijklmnopqrstuvwxyz'
+# even = alphabets[0: 10: 2]
+# print(even) #output: acegi
+# a, b, c, d = input()
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# output1 = "A single quote ' and a double quote \""
+# output2 = 'A forward slash / and a backward slash \\'
+# output3 = "Three single quotes ''' and three double quotes \"\"\""
+# output4 = "Double forward slash // and Double backward slash \\\\"
+
+# print(output1,output2,output3,output4,sep='\n')
+# num = int(input())
+# first, middle, last = int(num[0]), int(num[1]), int(num[2])
+# if first + last == middle:
+#     print('sandwich')
+# else:
+#     print('plain')
+# for i in range(1,11):
+#     print(i)
+# empty_list = []
+# for num in range(1,20):
+#     if num%2 == 0:
+#         print(num)
+#         empty_list.append(num)
+
+# print(len(empty_list))
+# total 
+# x = int(input())
+# y = 0
+# while x > 1:
+#     x = x // 2
+#     y = y + 1
+# print(y)
+
+# print(list(range(1, 10, 2)))
+# print(list(range(10, 0, -2)))
+# # print(list(range(1, 10, 2)))
+
+# alpha = 'abcdefghijklmnopqrstuvwxyz'
+# print(alpha.index('z'))
+# print(20%26)
+
+names = 'Albus Percival Brian Wulfric Dumbledore'
+for name in names:
+    print(name)
