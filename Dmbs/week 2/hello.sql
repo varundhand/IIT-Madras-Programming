15a1bee13d05d6a27d55a525e70d9cf557f8771b
Varun Dhand <varundhand77@gmail.com>
2024-10-21 14:28:58 +0530

    join statements

--- Dmbs/week 2/hello.sql
+++ Dmbs/week 2/hello.sql
@@ -52,3 +52,57 @@ select name from bands limit 2;
 
 -- as keyword is used to give an alias to the column
 select name as 'Band Name', id as 'Band ID' from bands;
+
+-- selecting in order
+select name as 'Band Names' from bands order by name;
+select name as 'Band Names' from bands order by name desc; -- for descending order
+
+-- insert multiple values
+insert into albums (name, release_year,band_id) 
+values ('Abbey Road',1969, 1),
+('Help!',1965, 1),
+('Aftermath', 1966,2),
+('The Works', 1984,3),
+('Test Album', NULL, 4);
+
+-- distinct keyword is used to select unique values 
+select distinct name  from albums;
+
+--! Update keyword is used to update/modify the data in the table (update, set, where)
+update albums 
+set release_year = 1966
+where id = 2;
+
+-- filtering the data using where clause
+select * from albums
+where release_year > 1980;
+
+-- like keyword is used to search for a specified pattern in a column
+select * from albums
+where name like '%the%';
+-- where name like '%the%' and band_id = 3;
+-- where release_year between 1960 and 1970; --! between is used to select the range of values
+-- where release_year is Null; --! is used to check if the value is null
+
+--! Delete keyword is used to delete the data from the table
+delete from albums 
+where id = 5;
+
+--!* JOINS
+--! inner join is used to combine rows from two or more tables based on a related column between them
+select * from bands 
+inner join albums on bands.id = albums.band_id;
+
+-- inner join shows the common rows between the two tables (same id in both tables)
+-- left join shows all the rows from the left table and the common rows from the right table
+-- right join shows all the rows from the right table and the common rows from the left table
+
+--! left join
+select * from bands 
+left join albums on bands.id = albums.band_id;
+
+--! right join
+select * from bands 
+right join albums on bands.id = albums.band_id;
+
+--!* Aggregate Functions
