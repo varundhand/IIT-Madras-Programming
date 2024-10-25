3afdb4d095e58f238b8ce8ab4fa6bb6401e7b983
Varun Dhand <varundhand77@gmail.com>
2024-10-25 15:24:32 +0530

    Data Types in Java

---
+++ Java/hello.java
@@ -0,0 +1,19 @@
+//* Data Types
+
+// Primitive Data Types
+// byte, short, int, long, float, double, boolean, char
+
+// Non-Primitive Data Types
+// String, Array, Class
+
+// 1. int
+// int myNum = 5;
+
+// integer has sub types i.e. byte, short, long
+// int is 4 bytes long (1 byte = 8 bits)
+// byte is 1 byte long
+// short is 2 bytes long
+// long is 8 bytes long
+
+// 2. float
+// float myFloatNum = 5.99f;
